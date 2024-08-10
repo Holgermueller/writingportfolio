3faf2308666ab8819f4d58959586da2752fc0bd8
holgermueller <9jholger@gmail.com>
2024-08-10 13:44:26 -0500

    set up file structure

---
+++ compnents/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div>
+      <footer>
+        <p>&copy; Copyright 2024 Holger Mueller</p>
+      </footer>
+    </div>
+  );
+}
---
+++ compnents/nav.js
@@ -0,0 +1,7 @@
+export default function Nav() {
+  return (
+    <nav>
+      <p>This is the nav.</p>
+    </nav>
+  );
+}
