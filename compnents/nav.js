782d5f42690a3738e7a124790960785f4660c45c
holgermueller <9jholger@gmail.com>
2024-08-11 14:34:01 -0500

    linked resume to site rather than have resume page

--- compnents/nav.js
+++ compnents/nav.js
@@ -5,7 +5,12 @@ export default function Nav() {
     <nav>
       <Link href="/">Home</Link>
       <Link href="/portfolio">Portfolio</Link>
-      <Link href="/resume">Resume</Link>
+      <Link
+        href="https://docs.google.com/document/d/1DL3GknJQjpYUSsOTx1caH-1cQA_fkOP5NTfunBL87l8/edit?usp=sharing"
+        target="_blank"
+      >
+        Resume
+      </Link>
       <Link href="/contact">Contact</Link>
     </nav>
   );
